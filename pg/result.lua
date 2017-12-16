caecc46fd5d75b50722c8b1e118b35499b8b1ac5
daurnimator <quae@daurnimator.com>
2017-12-17 00:18:19 +1100

    pg/result.lua: Add support for copy-in and copy-out results

--- pg/result.lua
+++ pg/result.lua
@@ -13,6 +13,8 @@ local function new_result(pgpsql_library, res)
 	}, result_mt)
 end
 
+local return_result
+
 function result_mt:__len()
 	return self.raw_result:ntuples()
 end
@@ -42,8 +44,52 @@ function result:tuples()
 	return next_tuple, self, 0
 end
 
+local function get_conn_result(conn)
+	local res = conn.raw_connection:getResult()
+	if res == nil then
+		return nil
+	end
+	return new_result(conn.pgsql, res)
+end
+
+local function new_copy_out(conn)
+	return get_conn_result, conn
+end
+
+local copy_in = {}
+local copy_in_mt = {
+	__name = "pg copy-in result";
+	__index = copy_in;
+}
+
+function copy_in:write(data)
+	assert(not self.is_closed, "copy-in is closed")
+	local ok = self.conn.raw_connection:putCopyData(data)
+	if not ok then
+		return nil, self.conn.raw_connection:errorMessage(), self.conn.raw_connection:status()
+	end
+	return ok
+end
+
+function copy_in:close(err)
+	assert(not self.is_closed, "copy-in is closed")
+	local ok = self.conn.raw_connection:putCopyEnd(err)
+	if not ok then
+		return nil, self.conn.raw_connection:errorMessage(), self.conn.raw_connection:status()
+	end
+	self.is_closed = true
+	return return_result(self.conn, self.conn.raw_connection:getResult())
+end
+
+local function new_copy_in(conn)
+	return setmetatable({
+		conn = conn;
+		is_closed = false;
+	}, copy_in_mt);
+end
+
 -- Helper for returning resultss
-local function return_result(conn, res)
+return_result = function (conn, res)
 	if not res then
 		return nil, conn.raw_connection:errorMessage(), conn.raw_connection:status()
 	end
@@ -59,6 +105,10 @@ local function return_result(conn, res)
 		end
 		-- TODO: free result ASAP https://github.com/arcapos/luapgsql/issues/42
 		return cmdStatus, tuples, oid
+	elseif status == conn.pgsql.PGRES_COPY_IN then
+		return new_copy_in(conn)
+	elseif status == conn.pgsql.PGRES_COPY_OUT then
+		return new_copy_out(conn)
 	else
 		local errmsg = res:errorMessage()
 		-- TODO: free result ASAP https://github.com/arcapos/luapgsql/issues/42
